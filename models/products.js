8af757dc2e613e42af055944b31b3f0df4d1de7a
Bernie Zhong <chu23008@byui.edu>
2025-11-18 18:28:02 +0800

    add more infor

--- models/products.js
+++ models/products.js
@@ -14,6 +14,6 @@ const productSchema = new Schema({
     stock: { 
         type: Number, required: true 
     },
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('Product', productSchema);
